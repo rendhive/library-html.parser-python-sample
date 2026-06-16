03abbe32302e792a90788b9976d7b0740ef92012
rendhive <rendhivecode@gmail.com>
2026-06-16 13:49:32 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul html.parser.py

---
+++ 1. Mengimpor Modul html.parser.py
@@ -0,0 +1,5 @@
+from html.parser import HTMLParser
+
+print("Modul html.parser berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk memanfaatkan parser HTML.
+# Kondisi: Digunakan sebelum menggunakan kelas `HTMLParser` untuk mem-pars data HTML.
